926c7dd6caa2a0d9cc5786c67f510a5b8951d54c
Ifeoluwa <deleoyeroifeoluwa@gmail.com>
2025-03-11 15:44:17 +0100

    Fixed issue with react query

--- src/hooks/useEvents.ts
+++ src/hooks/useEvents.ts
@@ -1,6 +1,6 @@
-import { useQuery } from "react-query";
 import { fetchEvents, fetchEventById } from "../api/eventsApi";
 import { EventsResponse, Event } from "../types/eventTypes";
+import { useQuery } from "@tanstack/react-query";
 
 export const useEvents = (
   page: number,
@@ -8,15 +8,18 @@ export const useEvents = (
   search: string,
   petsAllowed: boolean
 ) => {
-  return useQuery<EventsResponse>(
-    ["events", page, limit, search, petsAllowed],
-    () => fetchEvents(page, limit, search, petsAllowed)
-  );
+  return useQuery<EventsResponse>({
+    queryKey: ["events", page, limit, search, petsAllowed],
+    queryFn: () => fetchEvents(page, limit, search, petsAllowed),
+  });
 };
 
 export const useEventById = (id: string) => {
-  return useQuery<Event>(["event", id], async () => {
-    const event = await fetchEventById(id);
-    return event as unknown as Event;
+  return useQuery<Event>({
+    queryKey: ["event", id],
+    queryFn: async () => {
+      const event = await fetchEventById(id);
+      return event as unknown as Event;
+    },
   });
 };
